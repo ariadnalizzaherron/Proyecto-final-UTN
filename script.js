7004dc60ab035dd4efee37a7057859577ee8aaef
ariadnalizzaherron <ariadna.lizza@gmail.com>
2023-07-26 17:48:17 -0300

    Modificaciones

--- script.js
+++ script.js
@@ -1,37 +1,25 @@
-let video= document.querySelector('video');
-let video2= document.querySelector("#video-2")
+let video=document.querySelector('video');
+
+console.dir(video)
 
 setTimeout(()=>{    
     document.querySelector('.duracion').textContent='00:'+video.duration.toFixed(0)
 },2000)
 
 let repro;
-const reproducir=()=>{
-    video.play();
-    let duracionVideo=video.duration.toFixed(0);
 
-   repro= setInterval(()=>{
-    document.querySelector('.tiempo-actual').textContent='00:'+video.currentTime.toFixed(0)
-   },1000)
+const reproducir=()=> {
+    video.play()
+  let duracionVideo=video.duration.toFixed(0);
+ 
+  repro= setInterval (()=>{
+     document.querySelector('.tiempo-actual').textContent='00:'+video.currentTime.toFixed(0)
+ },1000)
 }
 
 const pausar=()=>{
-    video.pause();
-    clearInterval(repro)
-  
+ video.pause();
+ clearInterval(repro)
+ document.querySelector('.tiempo-actual').textContent=video.currentTime
 }
 
-
-const verDuracion=()=>{
-    let duracion=video2.duration
- console.log(duracion)
- if(duracion >= 60){
-   let minutos= parseInt(duracion/ 60 )
-   console.log(minutos);
-   let segundos =parseInt((duracion - minutos*60))
-    console.log(segundos)
-
-    document.querySelector(".duracion-2").textContent=minutos +":"+ segundos;
-
- }
-}
